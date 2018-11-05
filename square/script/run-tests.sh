925f96b7a3711e2276b5873b8dcfec731359a43b
Michael Pawliszyn <mikepaw@squareup.com>
2018-11-05 15:56:56 -0500

    Squares build script
    Adds Kochiku Docker building to Square's Vitess.
    Build docker image for Cloud Vitess
    Push docker images to us-west-2
    Use cash-ci script to push to ECR
    Upload JDBC driver to artifactory
    Run the integration tests
    Produce binaries (Linux and OSX) as part of the build
    Make binaries available on hoistrepo
    Building the boostrap images in order to not miss bootstrap changes.
    Run more tests in Kochiku
    Script nit picking
    Shard test running
    Make sure our scripts work with the go mod system
    Made script easier to follow
    Retry failed test builds
    
    Signed-off-by: Andres Taylor <antaylor@squareup.com>
    Signed-off-by: Yun Lai <ylai@squareup.com>

---
+++ square/script/run-tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Runs tests
+set -euxo pipefail
+
+if [[ -z ${KOCHIKU_ENV+x} ]]; then
+  echo "running in local mode"
+  GIT_COMMIT=$(git rev-parse HEAD)
+fi
+
+BUILD_DOCKER_TAG=square-vitess-build-${GIT_COMMIT}
+
+if [[ -n ${KOCHIKU_ENV+x} ]]; then
+  # if we are running inside of kochiku, first load the docker container
+  docker load -i docker-cache/"${BUILD_DOCKER_TAG}".tar
+fi
+
+docker run "$BUILD_DOCKER_TAG" go run test.go -docker=false -timeout=8m -print-log -shard "$1"
